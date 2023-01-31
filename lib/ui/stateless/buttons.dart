1b42a46dc506e8c6b00a5fb7d4ae25f7257342fb
ahmed <a.elmoselhy@gmail.com>
2023-01-31 17:15:20 +0200

    Init

--- lib/ui/stateless/buttons.dart
+++ lib/ui/stateless/buttons.dart
@@ -8,11 +8,20 @@ class Buttons extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Container(
-          width: 100,
-          height: 80,
+          width: 120,
+          height: 50,
           decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(10.0)),
-          child: Text("Ahmed"),
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey, offset: Offset(1, 1), blurRadius: 3),
+              ]),
+          alignment: Alignment.center,
+          child: Text(
+            "Ahmed",
+            style: TextStyle(fontSize: 30.0),
+          ),
         ),
       ),
     );
